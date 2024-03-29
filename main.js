5b89393953e84e22792554a1e5c57742ed143111
lenox <97343775+Brownred@users.noreply.github.com>
2024-03-29 07:57:14 +0000

    modified:   main.js

--- main.js
+++ main.js
@@ -62,7 +62,22 @@ class Ball {
       
         this.x += this.velX;
         this.y += this.velY;
-    }     
+    }   
+    
+    collisionDetect() {
+        for (const ball of balls) {
+          if (this !== ball) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+      
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+          }
+        }
+      }
+      
   }
 
   const balls = [];
@@ -90,10 +105,12 @@ class Ball {
     for (const ball of balls) {
       ball.draw();
       ball.update();
+      ball.collisionDetect();
     }
   
     requestAnimationFrame(loop);
   }
+  
 
 loop();
 
