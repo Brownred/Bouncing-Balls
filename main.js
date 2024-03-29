042496412299bf271ceee5b393501a4aaee6ad2b
lenox <97343775+Brownred@users.noreply.github.com>
2024-03-29 07:44:54 +0000

    modified:   index.html 	modified:   added functionality to main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // setup canvas
 
+// Get reference from the canva element
 const canvas = document.querySelector("canvas");
+
+// Gives us a context on which we can start to draw
+// ctx variable is the drawing area
+// We can only draw 2d objects as specified in the getContext argument
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
@@ -17,3 +22,26 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+
+class Ball {
+    constructor(x, y, velX, velY, color, size) {
+    // x and y coordinates — the horizontal and vertical coordinates
+      this.x = x;
+      this.y = y;
+    // horizontal and vertical velocity (velX and velY)
+      this.velX = velX;
+      this.velY = velY;
+      this.color = color;
+      this.size = size;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+      
+  }
+  
